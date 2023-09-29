3dfbc19433179264d08d869aac516aa579c42e47
Erik H <erik.hedrenius@mobileinteraction.se>
2023-09-29 13:44:59 +0200

    Update

--- Package.swift
+++ Package.swift
@@ -15,8 +15,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "PowerfoyleCore",
-            url: "https://maven.exeger.com/repository/maven-releases/com/exeger/powerfoyle/core-xcframework/1.2.2/core-xcframework-1.2.2.zip",
-            checksum: "d007efee6a31dd9b4a976c38c470789c5ebf5c0d79d89d7d55b34f226416a017"
+            url: "https://maven.exeger.com/repository/maven-releases/com/exeger/powerfoyle/core-xcframework/1.2.4/core-xcframework-1.2.4.zip",
+            checksum: "c5d6ffbf6be0f393880d496083dd3a5bfc639dda111e04fb7ebb1ade7c079f94"
         ),
     ]
 )
